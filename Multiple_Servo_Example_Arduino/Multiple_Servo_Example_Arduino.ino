70d8305b66c6fe15fdb66b60d401088e8f73b062
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-08-22 21:47:03 +0700

    feat: Servo done

--- Multiple_Servo_Example_Arduino/Multiple_Servo_Example_Arduino.ino
+++ Multiple_Servo_Example_Arduino/Multiple_Servo_Example_Arduino.ino
@@ -40,107 +40,104 @@ Servo servo2;
 Servo servo3;
 
 // Published values for SG90 servos; adjust if needed
-int minUs = 1000;
-int maxUs = 2000;
+int minUs = 550;
+int maxUs = 2400;
 
-// These are all GPIO pins on the ESP32
-// Recommended pins include 2,4,12-19,21-23,25-27,32-33
-// for the ESP32-S2 the GPIO pins are 1-21,26,33-42
-// for the ESP32-S3 the GPIO pins are 1-21,35-45,47-48
-// for the ESP32-C3 the GPIO pins are 1-10,18-21
 int servo1Pin = 12;
 int servo2Pin = 14;
 int servo3Pin = 27;
 
-int pos = 0;      // position in degrees
-ESP32PWM pwm;
+int pos = 0;
 void setup() {
-	// Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	Serial.begin(115200);
-	servo1.setPeriodHertz(50);      // Standard 50hz servo
-	servo2.setPeriodHertz(50);      // Standard 50hz servo
-	servo3.setPeriodHertz(50);      // Standard 50hz servo
-	// servo4.setPeriodHertz(200);      // Standard 50hz servo
-	//servo5.setPeriodHertz(50);      // Standard 50hz servo
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  Serial.begin(115200);
 
+  servo1.setPeriodHertz(50);  // Standard 50hz servo
+  servo2.setPeriodHertz(50);  // Standard 50hz servo
+  servo3.setPeriodHertz(50);  // Standard 50hz servo
 
+  resetServo(servo1, servo1Pin);
+  resetServo(servo2, servo2Pin);
+  resetServo(servo3, servo3Pin);
+
+  randomSeed(100);
 }
 
 void loop() {
-	servo1.attach(servo1Pin, minUs, maxUs);
-	servo2.attach(servo2Pin, minUs, maxUs);
-// #if defined(CONFIG_IDF_TARGET_ESP32S2) || defined(CONFIG_IDF_TARGET_ESP32C3)
-// 	pwm.attachPin(37, 10000);//10khz
-// #elif defined(CONFIG_IDF_TARGET_ESP32C3)
-// 	pwm.attachPin(7, 10000);//10khz
-// #else
-	pwm.attachPin(27, 10000);//10khz
-// #endif
-	servo3.attach(servo3Pin, minUs, maxUs);
-	// servo4.attach(servo4Pin, minUs, maxUs);
+  long randomNum;
+  randomNum = random(4);
 
-	//servo5.attach(servo5Pin, minUs, maxUs);
+  Serial.print("Random Choice: ");
+  Serial.println(randomNum);
+  
+  switch (randomNum) {
+    case 0:
+      rotateRightServo(servo1, servo1Pin);
+      rotateLeftServo(servo2, servo2Pin);
+      Serial.println("Waiting for the trash to fall down...");
+      delay(5000);
+      resetServo(servo1, servo1Pin);
+      resetServo(servo2, servo2Pin);
+      break;
+    case 1:
+      rotateLeftServo(servo1, servo1Pin);
+      rotateLeftServo(servo2, servo2Pin);
+      Serial.println("Waiting for the trash to fall down...");
+      delay(5000);
+      resetServo(servo1, servo1Pin);
+      resetServo(servo2, servo2Pin);
+      break;
+    case 2:
+      rotateRightServo(servo3, servo3Pin);
+      rotateRightServo(servo2, servo2Pin);
+      Serial.println("Waiting for the trash to fall down...");
+      delay(5000);
+      resetServo(servo3, servo3Pin);
+      resetServo(servo2, servo2Pin);
+      break;
+    case 3:
+      rotateLeftServo(servo3, servo3Pin);
+      rotateRightServo(servo2, servo2Pin);
+      Serial.println("Waiting for the trash to fall down...");
+      delay(5000);
+      resetServo(servo3, servo3Pin);
+      resetServo(servo2, servo2Pin);
+      break;
+  }
 
+  Serial.println("Completed!");
 
-	for (pos = 0; pos <= 180; pos += 1) { // sweep from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		servo1.write(pos);
-		delay(1);             // waits 20ms for the servo to reach the position
-	}
-	for (pos = 180; pos >= 0; pos -= 1) { // sweep from 180 degrees to 0 degrees
-		servo1.write(pos);
-		delay(1);
-	}
+  servo1.detach();
+  servo2.detach();
+  servo3.detach();
 
-	for (pos = 0; pos <= 180; pos += 1) { // sweep from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		servo2.write(pos);
-		delay(1);             // waits 20ms for the servo to reach the position
-	}
-	for (pos = 180; pos >= 0; pos -= 1) { // sweep from 180 degrees to 0 degrees
-		servo2.write(pos);
-		delay(1);
-	}
-
-	for (pos = 0; pos <= 180; pos += 1) { // sweep from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		servo3.write(pos);
-		delay(1);             // waits 20ms for the servo to reach the position
-	}
-	for (pos = 180; pos >= 0; pos -= 1) { // sweep from 180 degrees to 0 degrees
-		servo3.write(pos);
-		delay(1);
-	}
-
-	// for (pos = 0; pos <= 180; pos += 1) { // sweep from 0 degrees to 180 degrees
-	// 	// in steps of 1 degree
-	// 	servo4.write(pos);
-	// 	delay(1);             // waits 20ms for the servo to reach the position
-	// }
-	// for (pos = 180; pos >= 0; pos -= 1) { // sweep from 180 degrees to 0 degrees
-	// 	servo4.write(pos);
-	// 	delay(1);
-	// }
-	// for (pos = 0; pos <= 180; pos += 1) { // sweep from 0 degrees to 180 degrees
-	// 	// in steps of 1 degree
-	// 	servo5.write(pos);
-	// 	delay(1);             // waits 20ms for the servo to reach the position
-	// }
-	// for (pos = 180; pos >= 0; pos -= 1) { // sweep from 180 degrees to 0 degrees
-	// 	servo5.write(pos);
-	// 	delay(1);
-	// }
-	servo1.detach();
-	servo2.detach();;
-	servo3.detach();
-	// servo4.detach();
-	pwm.detachPin(27);
-
-	delay(5000);
-
+  delay(5000);
 }
 
+void rotateRightServo(Servo servo, int servoPin) {
+  servo.attach(servoPin, minUs, maxUs);
+  servo.write(0);
+  delay(1000);
+  Serial.println(servo.read());
+  servo.detach();
+}
+
+void rotateLeftServo(Servo servo, int servoPin) {
+  servo.attach(servoPin, minUs, maxUs);
+  servo.write(90);
+  delay(1000);
+  Serial.println(servo.read());
+  servo.detach();
+}
+
+void resetServo(Servo servo, int servoPin) {
+  servo.attach(servoPin, minUs, maxUs);
+  servo.write(45);
+  delay(1000);
+  Serial.println(servo.read());
+  servo.detach();
+}
